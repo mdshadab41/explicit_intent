a8fddf84511f1c8873971586391b924bb657bf10
Md Shadab <shadabjhu00447@gmail.com>
2022-06-23 13:10:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shadabexplicitintent/ResultActivity.java
@@ -0,0 +1,25 @@
+package com.example.shadabexplicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+
+        TextView result = (TextView) findViewById(R.id.resultView);
+
+        Intent intent = getIntent();
+
+        String addition = (String) intent.getSerializableExtra("SUM");
+        result.setText(addition);
+
+
+    }
+}
